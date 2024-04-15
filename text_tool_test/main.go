62e21066996ca7d841db2d74168d63b210d79903
David Ochiel <nguonodave@gmail.com>
2024-04-15 15:52:26 +0300

    seperated the custom functions from main file

---
+++ text_tool_test/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "goReloaded"
+
+/*
+--> after all modifications have been done, append words that are not instructions, i.e not "(up), (cap, 4) etc", to an empty array, "new_array_results
+--> write the sentence to the created file using io.WriteString
+--> defer output_file.Close() is for file handling
+*/
+func main() {
+	goReloaded.Modifications2()
+}
